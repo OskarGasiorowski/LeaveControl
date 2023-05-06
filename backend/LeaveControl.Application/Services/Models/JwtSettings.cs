21d6f760473e1f0830c16401261cdb43b5bc6cd8
OskarGasiorowski <41072665+OskarGasiorowski@users.noreply.github.com>
2023-05-06 19:02:59 +0200

    Warning fixes

--- backend/LeaveControl.Application/Services/Models/JwtSettings.cs
+++ backend/LeaveControl.Application/Services/Models/JwtSettings.cs
@@ -2,8 +2,8 @@ namespace LeaveControl.Application.Services.Models;
 
 public class JwtSettings
 {
-    public string Issuer { get; init; }
-    public string Audience { get; init; }
-    public string Key { get; init; }
+    public string Issuer { get; init; } = "";
+    public string Audience { get; init; } = "";
+    public string Key { get; init; } = "";
     public int ExpirationHours { get; init; }
 }
