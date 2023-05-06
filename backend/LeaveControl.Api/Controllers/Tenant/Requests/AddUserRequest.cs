a1470fe1ef0c2fc9723872da6d1f831c46b6a3f8
OskarGasiorowski <41072665+OskarGasiorowski@users.noreply.github.com>
2023-05-06 23:35:45 +0200

    Added validator for AddUserRequest

--- backend/LeaveControl.Api/Controllers/Tenant/Requests/AddUserRequest.cs
+++ backend/LeaveControl.Api/Controllers/Tenant/Requests/AddUserRequest.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 namespace LeaveControl.Api.Controllers.Tenant.Requests;
 
 public record AddUserRequest
@@ -5,4 +7,28 @@ public record AddUserRequest
     public string FirstName { get; init; } = "";
     public string Surname { get; init; } = "";
     public string Email { get; init; } = "";
+}
+
+public class AddUserRequestValidator : AbstractValidator<AddUserRequest>
+{
+    public AddUserRequestValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .WithMessage("First name is required.")
+            .MaximumLength(50)
+            .WithMessage("First name must be less than or equal to 50 characters.");
+
+        RuleFor(x => x.Surname)
+            .NotEmpty()
+            .WithMessage("Surname is required.")
+            .MaximumLength(50)
+            .WithMessage("Surname must be less than or equal to 50 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Email must be a valid email address.");
+    }
 }
