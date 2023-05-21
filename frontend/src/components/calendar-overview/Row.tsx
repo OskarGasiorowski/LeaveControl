a1787ea41adc80c4ac506ca953778d75e87b47c8
OskarGasiorowski <41072665+OskarGasiorowski@users.noreply.github.com>
2023-05-21 22:01:35 +0200

    highlight weekends

--- frontend/src/components/calendar-overview/Row.tsx
+++ frontend/src/components/calendar-overview/Row.tsx
@@ -58,13 +58,24 @@ export function Row({ displayName, leaves }: Props) {
             <Td borderStartRadius='lg' paddingY={3} paddingLeft={2} fontSize='sm'>
                 {displayName}
             </Td>
-            {calendar.map((type) => (
+            {calendar.map((type, index) => (
                 <>
-                    {!type && <EmptyCell />}
+                    {!type && (
+                        <EmptyCell
+                            isWeekend={
+                                new Date(2023, 4, index + 1).getDay() === 0 ||
+                                new Date(2023, 4, index + 1).getDay() === 6
+                            }
+                        />
+                    )}
                     {!!type && (
                         <LeaveCell
                             color={leaves.find((leave) => leave.id === type?.id)!.color}
                             variant={type!.variant}
+                            isWeekend={
+                                new Date(2023, 4, index + 1).getDay() === 0 ||
+                                new Date(2023, 4, index + 1).getDay() === 6
+                            }
                         />
                     )}
                 </>
@@ -73,12 +84,13 @@ export function Row({ displayName, leaves }: Props) {
     );
 }
 
-function EmptyCell() {
+function EmptyCell({ isWeekend }: { isWeekend: boolean }) {
     return (
         <Td
             borderRight='1px solid rgba(228, 228, 228, 0.04)'
             _last={{ borderEndRadius: 'lg', border: 'none' }}
             paddingX={1}
+            backgroundColor={isWeekend ? '#232633' : 'unset'}
         />
     );
 }
@@ -86,9 +98,11 @@ function EmptyCell() {
 function LeaveCell({
     color,
     variant,
+    isWeekend,
 }: {
     color: string;
     variant: 'left' | 'center' | 'right' | 'single';
+    isWeekend: boolean;
 }) {
     const variants: Record<
         typeof variant,
@@ -106,8 +120,9 @@ function LeaveCell({
             _last={{ borderEndRadius: 'lg', paddingRight: 2, border: 'none' }}
             paddingX={0}
             paddingY={0}
-            paddingLeft={variant === 'left' ? 1 : 0}
-            paddingRight={variant === 'right' ? 1 : 0}
+            paddingLeft={variant === 'left' || variant === 'single' ? 1 : 0}
+            paddingRight={variant === 'right' || variant === 'single' ? 1 : 0}
+            backgroundColor={isWeekend ? '#232633' : 'unset'}
         >
             <Box
                 {...variants[variant]}
