3e00df189a0f2e090246cf4636d976cc894f9ef2
OskarGasiorowski <oskar@gasiorowski.ovh>
2023-07-03 00:26:21 +0200

    theme/components/Link - proper file extension

--- frontend/src/theme/components/index.ts
+++ frontend/src/theme/components/index.ts
@@ -2,6 +2,6 @@ export { Alert } from './Alert';
 export { Button } from './Button';
 export { FormLabel } from './FormLabel';
 export { Input } from './Input';
-export { Link } from './Link';
+export { Link } from './Link.ts';
 export { Card } from './Card';
 export { Modal } from './Modal.ts';
